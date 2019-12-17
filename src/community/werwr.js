18931cf3008afda35d49bece46a4eb30e065287e
asd <norjapan21@gmail.com>
2019-12-17 19:07:39 +0900

    ㅁㄴㅇㅁㄴㅇ

---
+++ src/community/werwr.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class werwr extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default werwr;
