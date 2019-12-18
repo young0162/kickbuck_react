8e188ef8b224350c9592f1686b5cca031421b52d
universeandi <universeandi>
2019-12-18 15:47:18 +0900

    테스트용

---
+++ src/yoonju_test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div> 
+        yj 2
+        textInComponent </div>
+    );
+  }
+}
+
+
