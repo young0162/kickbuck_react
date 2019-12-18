1b55a04aa272f22c9e40dfe5b5f63d09296bcb1e
universeandi <universeandi>
2019-12-18 15:01:19 +0900

    test-yoonju

---
+++ src/test_1.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div> yoonju - textInComponent </div>
+    );
+  }
+}
+
