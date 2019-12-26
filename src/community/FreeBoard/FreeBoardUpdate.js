ef90ee78939ef0f160f1dc2419b49c54d9b1afe5
lang2s <lang2s@naver.com>
2019-12-26 16:30:01 +0900

    수정까지 완료

--- src/community/FreeBoard/FreeBoardUpdate.js
+++ src/community/FreeBoard/FreeBoardUpdate.js
@@ -2,16 +2,44 @@ import React, { Component, Fragment } from "react";
 import axios from "axios";
 
 export default class FreeBoardDetail extends Component {
-  constructor(props) {
-    super(props);
+  constructor({ history, match }) {
+    super();
+
+    this.history = history;
+    this.num = match.params.num;
     this.state = {
+      //스프링으로부터 dto 데이타를 받을변수
+      selectData: "",
       title: "",
-      imagename: "",
       content: "",
+      imagename: "",
       writer: "",
-      msg: "test"
+      num: this.num
     };
+
+    this.onSelect = this.onSelect.bind(this);
   }
+  //글선택 함수
+  onSelect = () => {
+    var url =
+      "http://localhost:8080/controller/community/freeboarddetail?num=" +
+      this.num;
+
+    axios
+      .get(url)
+      .then(responseData => {
+        console.log(responseData.data);
+        this.setState({
+          title: responseData.data.title,
+          content: responseData.data.content,
+          imagename: responseData.data.imagename,
+          writer: responseData.data.writer
+        });
+      })
+      .catch(error => {
+        console.log("select error");
+      });
+  };
 
   onImageUpload = e => {
     const uploadFile = e.target.files[0];
@@ -38,10 +66,18 @@ export default class FreeBoardDetail extends Component {
       });
   };
 
+  onKeyChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
   onSubmit = e => {
     e.preventDefault();
 
     const uploadFile = this.state;
+
+    console.log(this.num);
     var url =
       "http://localhost:8080/controller/community/freeboarddetail/update";
     axios
@@ -51,18 +87,21 @@ export default class FreeBoardDetail extends Component {
           msg: res.data,
           title: "",
           imagename: "",
-          content: "",
-          writer: ""
+          content: ""
         });
-        this.props.history.push("/community/freeboardlist");
+        console.log(res.data);
+        this.props.history.push("/community/freeboarddetail/" + this.num);
       })
       .catch(err => {
         console.log("submit 오류:" + err.data);
       });
   };
 
+  componentWillMount = () => {
+    this.onSelect();
+  };
+
   render() {
-    const url = "http://localhost:8080/controller/save/";
     return (
       <Fragment>
         <form onSubmit={this.onSubmit}>
@@ -70,13 +109,22 @@ export default class FreeBoardDetail extends Component {
             <tbody>
               <tr>
                 <th>제목</th>
-                <input type="text"></input>
+                <input
+                  type="text"
+                  value={this.state.title}
+                  onChange={this.onKeyChange}
+                  name="title"
+                ></input>
               </tr>
 
               <tr>
                 <th>내용</th>
                 <td>
-                  <textarea></textarea>
+                  <textarea
+                    value={this.state.content}
+                    onChange={this.onKeyChange}
+                    name="content"
+                  ></textarea>
                 </td>
               </tr>
               <tr>
@@ -87,23 +135,31 @@ export default class FreeBoardDetail extends Component {
               </tr>
               <tr>
                 <th>작성자</th>
-                <input type="text"></input>
+                <td>
+                  <input value={this.state.writer} readOnly></input>
+                </td>
               </tr>
               <tr>
                 <td colSpan="2">
                   <button type="submit">저장</button>
                 </td>
               </tr>
+              <tr>
+                <td colSpan="2">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.history.push(
+                        "/community/freeboarddetail/" + this.num
+                      );
+                    }}
+                  >
+                    취소
+                  </button>
+                </td>
+              </tr>
             </tbody>
           </table>
-          <button
-            type="button"
-            onClick={() => {
-              this.history.push("/community/freeboarddetail/" + this.num);
-            }}
-          >
-            취소
-          </button>
         </form>
       </Fragment>
     );
