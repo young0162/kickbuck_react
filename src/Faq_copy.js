29df9cd0fca9cc9252fb0e228bf0269965be07e5
universeandi <universeandi>
2019-12-27 23:01:23 +0900

    윤주 - 질문을 몇 개 추가했고 부트스트랩 관련 라이브러리 설치했지만 부트스트랩 아직 활용하지는 않음

--- src/Faq_copy.js
+++ src/Faq_copy.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import './faq.css';
 import Collapsible from 'react-collapsible';
-
+//import 'bootstrap/dist/css/bootstrap.css';
 
 export default class FAQ extends Component {
 
@@ -39,16 +39,70 @@ export default class FAQ extends Component {
                     </Collapsible>
               </li>
               <li>
-                  <Collapsible trigger="Q. 나이 제한은 없나요?" className="question">
+                  <Collapsible trigger="Q. 원하는 글을 찾으려면 어떻게 하면 되나요?" className="question">
                           <p className="answer">
-                          나이와 관계없이 누구나 참여할 수 있습니다.
+                          ~
                           </p>
                     </Collapsible>
               </li>
               <li>
-                  <Collapsible trigger="Q. 나이 제한은 없나요?" className="question">
+                  <Collapsible trigger="Q. 회원 가입에 실패했어요" className="question">
                           <p className="answer">
-                          나이와 관계없이 누구나 참여할 수 있습니다.
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 로그인에 실패했어요" className="question">
+                          <p className="answer">
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 이메일 인증에 문제가 있어요" className="question">
+                          <p className="answer">
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 이메일말고 다른 본인 인증 수단은 없나요?" className="question">
+                          <p className="answer">
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 원하는 글을 찾으려면 어떻게 하면 되나요?" className="question">
+                          <p className="answer">
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 디데이 날짜를 변경하고 싶어요" className="question">
+                          <p className="answer">
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 디데이가 지난 버킷리스트를 되살리고 싶어요" className="question">
+                          <p className="answer">
+                          ~
+                          </p>
+                    </Collapsible>
+              </li>
+              <li>
+                  <Collapsible trigger="Q. 사기를 당한 것 같아 걱정되어요" className="question">
+                          <p className="answer">
+                          가까운 경찰서 또는 사이버 안전국, KISA(인터넷침해대응센터)으로 신고해 주세요.
+                          <br/>경찰청 사이버 안전 지킴이 바로가기  http://www.police.go.kr/www/security/cyber.jsp
+                          <br/>KISA(인터넷침해대응센터) 바로가기 https://www.krcert.or.kr/consult/phishing.do
+                          <br/>
+                          <br/>사기당한 방법 및 수법을 카페에 알려, 제2의 피해자가 나오지 않도록 합니다.  
+                          <br/>사기꾼에게서 온 문자나 연락처, 주소 등 인적 사항을 알아낼 수 있는 정보는 보존하고, 많은 사람과 공유합니다.
                           </p>
                     </Collapsible>
               </li>
